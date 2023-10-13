c15c9e07cef98d135a346a082e58a9df0863d842
Danielle Luz <daniln2209@gmail.com>
2023-10-13 11:33:51 -0300

    feat: create capture audio logic

---
+++ src/main.py
@@ -0,0 +1,4 @@
+from utils.AudioUtils import AudioUtils
+
+audio_utils = AudioUtils()
+audio_utils.process_audio()
---
+++ src/utils/AudioUtils.py
@@ -0,0 +1,28 @@
+import speech_recognition
+
+class AudioUtils:
+  def __init__(self):
+     self.recognizer = speech_recognition.Recognizer()
+     self.audio = ''
+     self.spoken_text = ''
+  
+  def process_audio(self):
+    while self.spoken_text != 'parar':
+      self.listen_audio()
+      self.interpret_audio_with_language()
+
+  def listen_audio(self):
+    with speech_recognition.Microphone() as microphone:
+      print("Waiting audio input...")
+      self.recognizer.adjust_for_ambient_noise(microphone)
+      audio = self.recognizer.listen(microphone)
+      self.audio = audio
+    
+  def interpret_audio_with_language(self):
+      try:
+          self.spoken_text = self.recognizer.recognize_google(self.audio, language='pt-BR')
+          print(f"You said: {self.spoken_text}")
+      except speech_recognition.UnknownValueError:
+          print("It was not possible to understand the audio")
+      except speech_recognition.RequestError as e:
+          print(f"Error in the Google Speech Recognition request: {e}")
