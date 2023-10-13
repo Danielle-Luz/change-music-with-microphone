dded14235945b194dae60b38b849f49d18158413
Danielle Luz <daniln2209@gmail.com>
2023-10-13 11:43:58 -0300

    feat: create and use enviroment variables to make the audio process dynamic

--- src/main.py
+++ src/main.py
@@ -1,4 +1,7 @@
+from dotenv import load_dotenv
 from utils.AudioUtils import AudioUtils
+
+load_dotenv()
 
 audio_utils = AudioUtils()
 audio_utils.process_audio()
--- src/utils/AudioUtils.py
+++ src/utils/AudioUtils.py
@@ -1,4 +1,5 @@
 import speech_recognition
+import os
 
 class AudioUtils:
   def __init__(self):
@@ -7,9 +8,10 @@ class AudioUtils:
      self.spoken_text = ''
   
   def process_audio(self):
-    while self.spoken_text != 'parar':
+    STOP_COMMAND = os.getenv('STOP_COMMAND')
+    while self.spoken_text != STOP_COMMAND:
       self.listen_audio()
-      self.interpret_audio_with_language()
+      self.interpret_audio_with_spoken_language()
 
   def listen_audio(self):
     with speech_recognition.Microphone() as microphone:
@@ -18,9 +20,10 @@ class AudioUtils:
       audio = self.recognizer.listen(microphone)
       self.audio = audio
     
-  def interpret_audio_with_language(self):
+  def interpret_audio_with_spoken_language(self):
       try:
-          self.spoken_text = self.recognizer.recognize_google(self.audio, language='pt-BR')
+          SPOKEN_LANGUAGE = os.getenv('SPOKEN_LANGUAGE')
+          self.spoken_text = self.recognizer.recognize_google(self.audio, language=SPOKEN_LANGUAGE)
           print(f"You said: {self.spoken_text}")
       except speech_recognition.UnknownValueError:
           print("It was not possible to understand the audio")
